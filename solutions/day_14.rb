cdbd49d38cb8263f99b7728034bb5cd138cbf611
jpclark6 <jpclark6@gmail.com>
2018-12-24 20:53:25 -0800

    finishes day 14

--- solutions/day_14.rb
+++ solutions/day_14.rb
@@ -44,42 +44,45 @@ class Recipe
   def find_recipe_sequence(input)
     input = input.chars.map { |num| num.to_i }
     input_length = input.length
-    recipe_number = 0
 
     elf_1 = Elf.new(0, 3)
     elf_2 = Elf.new(1, 7)
     recipes = [3, 7]
-    current_length = 2
+    current_loc = 2
 
-    until recipes[-input_length] == input[-input_length] && recipes[-input_length + 1] == input[-input_length + 1] &&
-      recipes[-input_length + 2] == input[-input_length + 2] && recipes[-input_length + 3] == input[-input_length + 3] &&
-      recipes[-input_length + 4] == input[-input_length + 4] && recipes[-input_length..-1] == input
-
+    until recipes[current_loc - input_length] == input[-input_length] && recipes[current_loc + 1 - input_length] == input[-input_length + 1] &&
+      recipes[current_loc + 2 - input_length] == input[-input_length + 2] && recipes[current_loc + 3 - input_length] == input[-input_length + 3] &&
+      recipes[current_loc + 4 - input_length] == input[-input_length + 4] && recipes[current_loc + 5 - input_length] == input[-input_length + 5] &&
+      recipes[current_loc - input_length..current_loc - 1] == input
       next_recipe = find_next_recipe(elf_1, elf_2)
 
       if next_recipe > 9
-        next_recipe_1 = 1
-        next_recipe_2 = next_recipe - 10
-        recipes << next_recipe_1
-        recipes << next_recipe_2
-        next_recipe_length = 2
+        recipes << 1
+        current_loc += 1
+        if recipes[current_loc - input_length] == input[-input_length] && recipes[current_loc + 1 - input_length] == input[-input_length + 1] &&
+            recipes[current_loc + 2 - input_length] == input[-input_length + 2] && recipes[current_loc + 3 - input_length] == input[-input_length + 3] &&
+            recipes[current_loc + 4 - input_length] == input[-input_length + 4] && recipes[current_loc + 5 - input_length] == input[-input_length + 5] &&
+            recipes[current_loc - input_length..current_loc - 1] == input
+            break
+        end
+        recipes << next_recipe - 10
+        current_loc += 1
       else
         recipes << next_recipe
-        next_recipe_length = 1
+        current_loc += 1
       end
 
-      current_length += next_recipe_length
-      elf_1.loc = (elf_1.loc + elf_1.score + 1) % current_length
-      elf_2.loc = (elf_2.loc + elf_2.score + 1) % current_length
+      elf_1.loc = (elf_1.loc + elf_1.score + 1) % current_loc
+      elf_2.loc = (elf_2.loc + elf_2.score + 1) % current_loc
       elf_1.score = recipes[elf_1.loc]
       elf_2.score = recipes[elf_2.loc]
 
-      if current_length % 1000000 == 0
-        puts "Recipe: #{current_length}"
-      end
+      # if current_loc % 1_000_000 == 0
+      #   puts "Recipe: #{current_loc}"
+      # end
 
     end
-    current_length - input_length
+    current_loc - input_length
   end
 
 end
@@ -90,5 +93,5 @@ puzzle_input = 260321
 recipe_scores = recipe.find_recipes_after(puzzle_input)
 puts "Solution part 1: #{recipe_scores.map { |num| num.to_s }.join}"
 
-recipe_scores = recipe.find_recipe_sequence("51589")
+recipe_scores = recipe.find_recipe_sequence("260321")
 puts "Solution part 2: #{recipe_scores}"
